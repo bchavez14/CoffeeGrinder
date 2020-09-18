c4758817239230b82e5bbc253c9c72fca2f17e67
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-09-17 19:36:13 -0700

    initial project setup commit

---
+++ src/main/java/com/NuclearNode/DrinkApp/DrinkAppApplication.java
@@ -0,0 +1,13 @@
+package com.NuclearNode.DrinkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkAppApplication.class, args);
+	}
+
+}
